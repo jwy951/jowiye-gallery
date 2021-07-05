84cd067e138f414a31be44433155a572773794af
Santa claus <santa@nortpole.com>
2021-07-05 22:29:50 +0300

    add Procfile

--- gallery/settings.py
+++ gallery/settings.py
@@ -168,7 +168,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-TATICFILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
